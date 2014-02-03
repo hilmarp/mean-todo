8f34fa7030437ab314abd19ddfab90e34997bc97
Hilmar <hilmar@hilmarp.com>
2014-02-03 22:52:49 +0000

    get, post and delete api ready

--- server.js
+++ server.js
@@ -55,6 +55,26 @@ app.post("/api/todos", function (req, res) {
 	});
 });
 
+// Delete a todo
+app.delete("/api/todos/:todo_id", function (req, res) {
+	Todo.remove({
+		_id: req.params.todo_id
+	}, function (err, todo) {
+		if (err) {
+			res.send(err);
+		}
+
+		// Get and return all todos after deleting
+		Todo.find(function (err, todos) {
+			if (err) {
+				res.send(err);
+			}
+
+			res.json(todos);
+		});
+	});
+});
+
 // Listen
 app.listen(8080);
 console.log("App listening on port 8080");
